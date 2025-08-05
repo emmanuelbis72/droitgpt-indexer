ec9092e0058499b459deffadb2dcb1848339433a
Emmanuel Bisimwa <emmanuelbis72@gmail.com>
2025-08-05 18:47:37 +0300

    Sauvegarde locale avant pull

--- analyse-pdf/server.js
+++ analyse-pdf/server.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import OpenAI from 'openai';
-import createAnalyseDocumentRoute from './analyseDocument.js';
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const OpenAI = require('openai');
+const createAnalyseDocumentRoute = require('./analyseDocument');
 
 dotenv.config();
 
